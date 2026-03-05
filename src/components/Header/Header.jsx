799f6774d35d4b6678784e00cb6cc0c476ad679c
shaonmh <shaonshan41@gmail.com>
2026-03-05 23:36:32 +0600

    solve responsive issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
               </div>
               <ul
                 tabIndex="-1"
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow flex flex-col gap-3"
               >
                 <li>
                   <a>Home</a>
